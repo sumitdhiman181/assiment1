adb2c46d3e79b48cc1444b2b9215fd9f2d7b49b8
Sumit <sumitdhiman181@gmail.com>
2026-02-28 09:58:00 +0530

    Implement factorial and square root calculations
    Added factorial calculation and square root functionality.

--- assiment03.py
+++ assiment03.py
@@ -1,3 +1,4 @@
+import math
 def fact(num):
     factorial = 1
     while num > 1:
@@ -7,13 +8,9 @@ def fact(num):
 n = int(input("enter the number:"))
 print(f"factorical of {n} is {fact(n)}")
 
-
-import math
-
 num = float(input("enter the numner:"))
 output = math.sqrt(num)
 print(f"square root : {output}")
-
 
 log = math.log(num)
 print("Natural logarithm:", log)
